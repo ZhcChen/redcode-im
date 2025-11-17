ea20d038e899f9cfda787ead8ef3832d45bc8326
ZhcChen <chen1406736605@gmail.com>
2025-11-17 23:16:49 +0800

    fix(frontend): 调整自己发送的消息气泡最大宽度为80%

--- frontend/lib/features/chat/widgets/chat_message_bubble.dart
+++ frontend/lib/features/chat/widgets/chat_message_bubble.dart
@@ -59,7 +59,7 @@ class ChatMessageBubble extends StatelessWidget {
           ConstrainedBox(
             constraints: BoxConstraints(
               maxWidth: message.isSelf
-                  ? MediaQuery.of(context).size.width * 0.75
+                  ? MediaQuery.of(context).size.width * 0.8
                   : double.infinity,
             ),
             child: Container(
