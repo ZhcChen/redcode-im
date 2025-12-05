4557e1dda1806968f9d85f605f906d3c7cc7c4b5
ZhcChen <chen1406736605@gmail.com>
2025-12-05 15:15:37 +0800

    fix(desktop-message-pin): 再次调大置顶 Banner 图标尺寸

--- desktop/src/components/PinnedMessageBanner.vue
+++ desktop/src/components/PinnedMessageBanner.vue
@@ -217,8 +217,8 @@ const handleIconClick = () => {
 
 .pinned-icon-button svg {
   stroke: #555555;
-  width: 20px;
-  height: 20px;
+  width: 22px;
+  height: 22px;
 }
 
 .pinned-icon-button:hover svg {
